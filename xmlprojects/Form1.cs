8ad03d7abc3e478e1685baf0e0f90d5fcaa7b0ab
HP <HP@HP>
2023-08-29 20:37:37 +0300

    Proje dosyası ekle.

---
+++ xmlprojects/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace xmlprojects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement root = xmlDocument.CreateElement("Tedarikciler");
+
+            SqlConnection con = new SqlConnection("Server=.;Database=Northwind;Integrated Security=true;");
+            SqlCommand command = new SqlCommand("select*from Tedarikciler", con);
+            con.Open();
+            SqlDataReader reader=command.ExecuteReader();
+            while (reader.Read())
+            {
+                XmlElement tedarikci = xmlDocument.CreateElement("Tedarikci");
+                XmlAttribute adres = xmlDocument.CreateAttribute("Adres");
+                adres.Value = reader["Adres"].ToString();
+
+                XmlAttribute sehir = xmlDocument.CreateAttribute("Sehir");
+                sehir.Value = reader["Sehir"].ToString();
+
+                XmlAttribute ulke = xmlDocument.CreateAttribute("Ulke");
+                ulke.Value = reader["Ulke"].ToString();
+
+                tedarikci.Attributes.Append(adres);
+                tedarikci.Attributes.Append(sehir);
+                tedarikci.Attributes.Append(ulke);
+                root.AppendChild(tedarikci);
+
+            }
+
+            con.Close();
+            xmlDocument.AppendChild(root);
+            xmlDocument.Save("veri.xml");
+
+          
+
+        }
+    }
+}
